07a646efa578f56440c0b8dfcce36ef523752cb1
Kapil Viren Ahuja <kapil@howtoarchitect.io>
2023-11-07 09:45:35 +0530

    fix formatting

--- week 2/print_checker.py
+++ week 2/print_checker.py
@@ -1,17 +1,17 @@
 def print_checker(number):
     if number > 100:
-        print "Enter a number which is less than 100"
+        print("Enter a number which is less than 100")
         return
     
     if number > 1:
         for i in range(2, number):
             if (number % i) == 0:
-                print (f"{number} is not a prime number")
+                print(f"{number} is not a prime number")
                 break
         else:
-            print (f"{number} is a prime number")
+            print(f"{number} is a prime number")
     else:
-        print (f"{number} is not a prime number")
+        print(f"{number} is not a prime number")
 
 
 n = int(input ("Enter a number: "))
