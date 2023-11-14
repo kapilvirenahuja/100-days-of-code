82f3d32a688ce6d4a62db1a14ae91f594713bfdf
Kapil Viren Ahuja <kapil@howtoarchitect.io>
2023-11-14 11:56:52 +0530

    trying to use modules, renamed folders too

---
+++ commons/helpers.py
@@ -0,0 +1,3 @@
+def clear() -> None:
+    """Clear the terminal."""
+    print("\033[H\033[2J", end="", flush=True)
---
+++ week2/silent_auction.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append("/Users/kapil/Code/learn/100-days-of-code")
+
+from commons.helpers import clear
+from auction_art import logo
+
+print(logo)
+clear()
